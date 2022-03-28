9137713e6d154a64355063cccf9439d9b4ed4125
XMX5217 <xmx5217@MacBookPro.local>
2022-03-28 01:47:21 -0600

    Adding icon

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import {Helmet} from "react-helmet";
+import ICON from "./assets/portfolio.ico"
 
 const App = () => {
   return (
     <>
+      <Helmet>
+        <title>Portfolio</title>
+        <link rel="icon" href={ICON} />
+      </Helmet>
       <Header/>
       <Nav/>
       <About/>
